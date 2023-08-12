a011febefab07b8c04cc19b9a847eccc2523057d
zLuciel <82103687+zLuciel@users.noreply.github.com>
2023-08-12 01:31:38 -0500

    add date base

--- api/src/db.js
+++ api/src/db.js
@@ -7,6 +7,15 @@ const {DB_USER, DB_PASSWORD, DB_HOST,DB_PORT,DB_NAME} = process.env;
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Ignorar errores de certificado (opción insegura)
+    }
+  }
 });
 
 const basename = path.basename(__filename);
